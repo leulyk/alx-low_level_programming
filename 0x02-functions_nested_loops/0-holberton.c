a3a9fa3cf1589f69f06532b06665da6ecdf05f42
Leul Yiheyis <leulykpro@gmail.com>
2021-06-22 18:51:59 +0300

    print holberton followed by a new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * Description: print holberton followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
