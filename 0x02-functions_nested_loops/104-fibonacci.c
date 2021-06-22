2cb2c753d5b63c377978e408f926da25e8367f8d
Leul Yiheyis <leulykpro@gmail.com>
2021-06-22 21:49:35 +0300

    print the first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the first 98 fibonacci numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	float prev, curr, temp;
+	int i;
+
+	i = 1;
+	prev = 1;
+	curr = 2;
+	printf("%.0f, %.0f, ", prev, curr);
+	while (i <= 96)
+	{
+		temp = curr;
+		curr += prev;
+		prev = temp;
+		printf("%.0f", curr);
+		if (i != 96)
+			printf(", ");
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
