635a6304d71a9d081deac498d17fa5fe63e9da2d
Leul Yiheyis <leulykpro@gmail.com>
2021-06-22 19:41:46 +0300

    print the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,8 @@
  */
 int print_last_digit(int num)
 {
-	if (num >= 0)
-		return (num % 10);
-	else
-		return (-num % 10);
+	if (num < 0)
+		num = -num;
+	_putchar('0' + num % 10);
+	return (num % 10);
 }
