337a05b663a2a70e25641a331423386c0638260b
Leul Yiheyis <leulykpro@gmail.com>
2021-06-24 21:11:22 +0300

    Print the largest prime factor of 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int is_prime(long num);
+/**
+ * main - largest prime factor of the number 612852475143
+ * 
+ */
+int main(void)
+{
+    long int num, i, maxprime;
+    
+    num = 612852475143;
+    maxprime = 2;
+    for (i = 2; i <= num; ++i)
+    {
+        if (num % i == 0)
+        {
+            if (is_prime(i))
+            {
+                if (i > maxprime)
+                {
+                    maxprime = i;
+                }
+            }
+        }
+    }
+    printf("%li\n", maxprime);
+    return (0);
+}
+
+int is_prime(long num)
+{
+    long i;
+    
+    for (i = 2; i <= sqrt(num); ++i)
+    {
+        if (num % i == 0)
+        {
+            return (0);
+        }
+    }
+    return (1);
+}
