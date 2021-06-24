042cc67f6c0553dba059bfbd09d9822b06c48d8b
Leul Yiheyis <leulykpro@gmail.com>
2021-06-24 19:50:00 +0300

    print a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,21 +2,28 @@
 
 /**
  * print_diagonal - print a diagonal line
- * @n: number of diagonal
+ * @n: number of lines
  */
 void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		for (j = 1; j <= i; ++j)
-		{
-			if (j != i)
-				_putchar(' ');
-			else
-				_putchar('\\');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; ++i)
+		{
+			for (j = 1; j <= i; ++j)
+			{
+				if (j != i)
+					_putchar(' ');
+				else
+					_putchar('\\');
+			}
+			_putchar('\n');
+		}
 	}
 }
