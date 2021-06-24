64a03c0db4d02a67db7c9c795faa86794198d0d3
Leul Yiheyis <leulykpro@gmail.com>
2021-06-24 20:41:02 +0300

    Print triangle for a given size

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,4 +3,31 @@
 /**
  * print_triangle - print triangle for a given size
  * @size: height of triangle
- * /
+ */
+void print_triangle(int size)
+{
+    int i, j;
+    
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        for (i = size; i >= 1; --i)
+        {
+            for (j = 1; j <= size; ++j)
+            {
+                if (j >= i)
+                {
+                    _putchar('#');
+                }
+                else
+                {
+                    _putchar(' ');
+                }
+            }
+            _putchar('\n');
+        }
+    }
+}
