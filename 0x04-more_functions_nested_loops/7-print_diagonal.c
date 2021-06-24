66ec6497166cfc34d4ae5504a18eb10c9cf3de74
Leul Yiheyis <leulykpro@gmail.com>
2021-06-24 19:46:09 +0300

    print a diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ * @n: number of diagonal
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; ++j)
+		{
+			if (j != i)
+				_putchar(' ');
+			else
+				_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
