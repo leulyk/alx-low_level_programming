d304ebae99e9d7dd4cdaf07d61300ca69346122a
Leul Yiheyis <leulykpro@gmail.com>
2021-06-24 19:53:16 +0300

    print a square of a certain dimension

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_square - print square of a certain size
+ * @size: dimension
+ *
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; ++i)
+		{
+			for (j = 1; j <= size; ++j)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
