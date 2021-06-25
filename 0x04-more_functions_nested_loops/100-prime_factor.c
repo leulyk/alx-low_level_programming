670d2c654c1d566f2d0d3605b35d2b2536fc9714
Leul Yiheyis <leulykpro@gmail.com>
2021-06-25 20:17:43 +0300

    print the largest prime factor for 612852475143

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,42 +4,47 @@
 int is_prime(long num);
 /**
  * main - largest prime factor of the number 612852475143
- * 
+ * Return: 0 (success)
  */
 int main(void)
 {
-    long int num, i, maxprime;
-    
-    num = 612852475143;
-    maxprime = 2;
-    for (i = 2; i <= num; ++i)
-    {
-        if (num % i == 0)
-        {
-            if (is_prime(i))
-            {
-                num = num / i;
-                if (i > maxprime)
-                {
-                    maxprime = i;
-                }
-            }
-        }
-    }
-    printf("%li\n", maxprime);
-    return (0);
+	long int num, i, maxprime;
+
+	num = 612852475143;
+	maxprime = 2;
+	for (i = 2; i <= num; ++i)
+	{
+		if (num % i == 0)
+		{
+			if (is_prime(i))
+			{
+				num = num / i;
+				if (i > maxprime)
+				{
+					maxprime = i;
+				}
+			}
+		}
+	}
+	printf("%li\n", maxprime);
+	return (0);
 }
 
+/**
+ * is_prime - check whether a number is prime or not
+ * @num: the number to be checked
+ * Return: 1 if number is prime, otherwise 0
+ */
 int is_prime(long num)
 {
-    long i;
-    
-    for (i = 2; i <= sqrt(num); ++i)
-    {
-        if (num % i == 0)
-        {
-            return (0);
-        }
-    }
-    return (1);
+	long i;
+
+	for (i = 2; i <= sqrt(num); ++i)
+	{
+		if (num % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
