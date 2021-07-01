a71ccab6d83ada5bf0cd5630dba03ace6fdfebb0
Leul Yiheyis <leulykpro@gmail.com>
2021-07-01 08:50:18 +0300

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,24 +6,18 @@
  * @s1: the first string
  * @s2: the second string
  *
- * Return: 0 if strings are equal
- *         >0 if the first unmatched character of s1 is greater than s2
- *         <0 if the first unmatched character of s1 is less than s2
+ * Return: 0 if strings are equal otherwise the difference between the strings
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	for (; *s1 != '\0' && *s2 != '\0'; ++s1, ++s2)
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; ++i)
-	{
-		if (s1[i] > s2[i])
-			return (*s1 - *s2);
-		else
-			return (*s1 - *s2);
-	}
-	if (s1[i] != '\0')
+	if (*s1 != '\0')
 		return (*s1);
-	else if (s2[i] != '\0')
+	else if (*s2 != '\0')
 		return (*s2);
+
 	return (0);
 }
