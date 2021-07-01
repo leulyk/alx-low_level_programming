2126e5e5d037f75cc4eaea1c362c3472138e8e10
Leul Yiheyis <leulykpro@gmail.com>
2021-07-01 09:38:28 +0300

    capitalize word based on specified separators

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 
+int is_separator(char ch);
 /**
  * cap_string - capitalize all words of a string
  *
@@ -14,17 +15,34 @@ char *cap_string(char *str)
 {
 	int i, j;
 
-	for (j = 1; str[i] != '\0'; ++j)
+	for (j = 0; str[j] != '\0'; ++j)
 	{
 		i = j - 1;
-		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '.' ||
-			str[i] == ',' || str[i] == ';' || str[i] == '!' || str[i] == '?' ||
-				str[i] == '\"' || str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-					str[i] == '{')
+		if (is_separator(str[i]))
 		{
 			if (str[j] >= 'a' && str[i] <= 'z')
 				str[j] -= 32;
 		}		
 	}
 	return (str);
+}
+
+/**
+ * is_separator - check whether a character is a separator
+ *
+ * @ch: the character to be checked
+ *
+ * Return: 1 if it's a separator, otherwise 0
+ */
+int is_separator(char ch)
+{
+	char separators[] = " \n\t.,;!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; ++i)
+	{
+		if (separators[i] == ch)
+			return (1);
+	}
+	return (0);
 }
