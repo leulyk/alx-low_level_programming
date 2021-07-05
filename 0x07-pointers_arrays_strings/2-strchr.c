249dc09da240d21a1d09c54ca9dacee769302cef
Leul Yiheyis <leulykpro@gmail.com>
2021-07-05 14:39:07 +0300

    find a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,3 @@
-#include <stddef.h>
 #include "holberton.h"
 
 /**
@@ -17,5 +16,5 @@ char *_strchr(char *s, char c)
 	for (i = 0; *(s + i) != '\0'; ++i)
 		if (*(s + i) == c)
 			return (s + i);
-	return (NULL);
+	return (0);
 }
