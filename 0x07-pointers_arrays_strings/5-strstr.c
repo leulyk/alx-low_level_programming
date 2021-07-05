d54c9738dc2e9fbe00b96cc7e57498deae696090
Leul Yiheyis <leulykpro@gmail.com>
2021-07-05 19:22:49 +0300

    locate a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,20 +13,19 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, index;
 
-	for (i = 0; ; ++i)
+	for (i = 0; haystack[i] != '\0'; ++i)
 	{
-		if (*(haystack + i) == *(needle))
+		if (haystack[i] == needle[0])
 		{
 			index = i++;
-			for (j = 1; *(needle + j) != '\0' &&
-				(*(haystack + i) == *(needle + j)); ++i, ++j)
-				;
-			if (*(needle + j) == '\0')
+			for (j = 1; needle[j] != '\0' && haystack[i] != '\0'; ++i, ++j)
+			{
+				if (haystack[i] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
 				return (haystack + index);
 		}
-		if (*(haystack + i) == '\0')
-		{
-			return (NULL);
-		}
 	}
+	return (NULL);
 }
