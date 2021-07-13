98c7f6950ef32f57f0cecf361b2b154f4644542a
Leul Yiheyis <leulykpro@gmail.com>
2021-07-13 20:34:31 +0300

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,30 +15,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *fullstr;
-	int len1, len2, i, totallen;
+	char *fullstr, *empty;
+	int len1, len2, i, j;
+
+	empty = "";
+	if (s1 == NULL)
+		s1 = empty;
+	if (s1 == NULL)
+		s2 = empty;
 
 	len1 = len2 = 0;
-	if (s1 == NULL)
-		*s1 = '\0';
-	if (s1 == NULL)
-		*s2 = '\0';
-	while (*(s1 + len1))
+	while (s1[len1] != '\0')
 		len1++;
-	while (*(s2 + len2))
+	while (s1[len2] != '\0')
 		len2++;
-	totallen = len1 + len2;
-	fullstr = malloc((totallen + 1)  * sizeof(char));
+
+	fullstr = malloc(sizeof(*s1) * (len1 + len2 + 1));
 	if (fullstr == NULL)
 		return (NULL);
 
-	i = 0;
-	while (*s1)
-		*(fullstr + i++) = *s1++;
-	while (*s2)
-		*(fullstr + i++) = *s2++;
-
-	if (!i)
-		return (NULL);
+	i = j = 0;
+	while (i < len1)
+	{
+		fullstr[i] = s1[i];
+		++i;
+	}
+	while (j <= len2)
+	{
+		fullstr[i] = s2[j];
+		++j;
+		++i;
+	}
 	return (fullstr);
 }
