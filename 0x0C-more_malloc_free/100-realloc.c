b528eff0015c18c21bf12bfa08a0260aaed475d6
Leul Yiheyis <leulykpro@gmail.com>
2021-07-15 20:08:45 +0300

    reallocate a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,6 +29,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		free(ptr);
+		return (new_ptr);
+	}
 
 	min_size = old_size < new_size ? old_size : new_size;
 	new_ptr = malloc(new_size);
