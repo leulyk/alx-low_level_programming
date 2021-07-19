e5d5a80b3a8ede5fc65f33f10d45958d5c7af59e
Leul Yiheyis <leulykpro@gmail.com>
2021-07-19 16:22:12 +0300

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "dog.h"
 
+char *_strdup(char *str);
+
 /**
  * new_dog - create a new dog
  *
@@ -14,12 +16,59 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dg;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
 	dg = malloc(sizeof(dg));
 	if (dg == NULL)
 		return (NULL);
-	dg->name = name;
+	dg->name = _strdup(name);
+	if (dg->name == NULL)
+	{
+		free(dg);
+		return (NULL);
+	}
 	dg->age = age;
-	dg->owner = owner;
+	dg->owner = _strdup(owner);
+	if (dg->owner == NULL)
+	{
+		free(dg);
+		return (NULL);
+	}
 
 	return (dg);
+}
+
+
+/**
+ * _strdup - duplicate a string
+ *
+ * @str: the string to copy
+ *
+ * Return: pointer to the newly allocated space in memory,
+ *	   NULL if @str is null, or insufficient memory available
+ */
+char *_strdup(char *str)
+{
+	char *newstr;
+	int i, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (*(str + len++))
+		;
+	newstr = malloc(len * sizeof(char));
+	if (newstr == NULL)
+		return (NULL);
+
+	i = 0;
+	while (*(str + i))
+	{
+		*(newstr + i) = *(str + i);
+		++i;
+	}
+	*(newstr + i) = '\0';
+	return (newstr);
 }
