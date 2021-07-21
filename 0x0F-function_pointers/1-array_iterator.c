6c595959ec11d85c0a7fbb8de8145acea53109b6
Leul Yiheyis <leulykpro@gmail.com>
2021-07-21 15:45:20 +0300

    [Minor Fix] Check if array is null

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action != 0)
+	if (action != 0 && array != 0)
 	{
 		for (i = 0; i < size; ++i)
 			action(array[i]);
