4dde5f1361837cb6036fb6e466582972ba1d3bf2
Leul Yiheyis <leulykpro@gmail.com>
2021-07-21 17:29:42 +0300

    return an operation function by looking at the operand

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -27,31 +27,9 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while ((ops[i]).op != NULL)
 	{
-		if (!(_strcmp(ops[i].op, s)))
+		if (ops[i].op[0] == s[0])
 			return ((ops[i]).f);
 		++i;
 	}
-	return (0);
-}
-
-/**
- * _strcmp - compare two strings (exactly like the standard library function)
- *
- * @s1: the first string
- * @s2: the second string
- *
- * Return: 0 if strings are equal otherwise the difference between the strings
- */
-int _strcmp(char *s1, char *s2)
-{
-	for (; *s1 != '\0' && *s2 != '\0'; ++s1, ++s2)
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-
-	if (*s1 != '\0')
-		return (*s1);
-	else if (*s2 != '\0')
-		return (*s2);
-
 	return (0);
 }
