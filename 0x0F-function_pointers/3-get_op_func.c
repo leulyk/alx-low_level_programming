d3426b1ca6f9cb81c771ad856dc4007e6bac7862
Leul Yiheyis <leulykpro@gmail.com>
2021-07-21 17:42:01 +0300

    return an operation function by checking on the operand

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,8 +22,6 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	if (s == 0)
-		return (0);
 	i = 0;
 	while ((ops[i]).op != NULL)
 	{
@@ -31,5 +29,5 @@ int (*get_op_func(char *s))(int, int)
 			return ((ops[i]).f);
 		++i;
 	}
-	return (0);
+	return (NULL);
 }
