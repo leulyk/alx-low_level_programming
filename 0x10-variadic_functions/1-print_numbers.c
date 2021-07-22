dd37843c43194e95e825e4f9ad12436420341040
Leul Yiheyis <leulykpro@gmail.com>
2021-07-22 19:24:05 +0300

    [Fix] add newline at the end of the function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	if (separator != NULL)
+	if (separator != NULL && n > 0)
 	{
 		va_start(list, n);
 		for (i = 0; i < n; ++i)
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (i != n - 1)
 				printf("%s", separator);
 		}
-		printf("\n");
 		va_end(list);
 	}
+	printf("\n");
 }
