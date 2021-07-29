6b2fab0684a46bdbcee9e92338b5844689d57fef
Leul Yiheyis <leulykpro@gmail.com>
2021-07-29 17:59:28 +0300

    Fix on betty documentation error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,7 @@
  * print_first - a function to be executed before main
  *
  */
-__attribute__ ((__constructor__))
-void print_first(void)
+void __attribute__ ((__constructor__)) print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
