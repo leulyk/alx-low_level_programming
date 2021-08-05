f1dd1b43457e4f56aabb942ab6f41ffcad15b2d2
Leul Yiheyis <leulykpro@gmail.com>
2021-08-05 21:12:18 +0300

    check endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - check the endianness of one's machine
+ *
+ * Return: 1 if it's little endian, 0 if it's big endian
+ *
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *ch;
+
+	ch = (char *)&i;
+	if (*ch)
+		return (1);
+	return (0);
+}
