16f9e38de87614b13569608141820c1218034a25
Leul Yiheyis <leulykpro@gmail.com>
2021-08-05 20:57:49 +0300

    number of bits to be flipped to get one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - compute the number of bits needed to flip to get from one
+ * number to another
+ *
+ * @num1: first number
+ * @num2: second numer
+ *
+ * Return: number of bits to be flipped
+ */
+unsigned int flip_bits(unsigned long int num1, unsigned long int num2)
+{
+	int index, bit1, bit2, count;
+
+	count = 0;
+	for (index = 31; index >= 0; --index)
+	{
+		bit1 = (num1 & (1 << index)) != 0;
+		bit2 = (num2 & (1 << index)) != 0;
+		if (bit1 != bit2)
+			count++;
+	}
+	return (count);
+}
