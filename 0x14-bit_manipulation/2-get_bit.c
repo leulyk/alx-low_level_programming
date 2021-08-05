e95b4725fd927ae649ecaddebb01675b721419c9
Leul Yiheyis <leulykpro@gmail.com>
2021-08-05 20:28:13 +0300

    return the bit value at a specified index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - get the bit a specified index
+ *
+ * @num: number to get the bit from
+ * @index: index of the bit to get
+ *
+ * Return: value of the bit at index, otherwise -1
+ */
+int get_bit(unsigned long int num, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index > 63)
+		return (-1);
+	mask = 1 << index;
+
+	return ((num & mask) != 0);
+}
