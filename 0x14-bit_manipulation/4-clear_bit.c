6cd3426bcd2b32f403aaf9cf62cabe1aa8b5a01d
Leul Yiheyis <leulykpro@gmail.com>
2021-08-05 20:45:20 +0300

    set a bit at a specified position to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - set a bit at a specified position to 0
+ *
+ * @num: pointer to an integer
+ * @index: index to set bit
+ *
+ * Return: 1 if it worked, otherwise -1
+ *
+ */
+int clear_bit(unsigned long int *num, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*num &= ~(1 << index);
+
+	return (1);
+}
