99d0f195be662f659d5ab7684f2f01977ae589f5
Leul Yiheyis <leulykpro@gmail.com>
2021-08-09 21:14:16 +0300

    Add write only flag

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	long length, write_count;
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1 || filename == NULL)
 		return (-1);
 
