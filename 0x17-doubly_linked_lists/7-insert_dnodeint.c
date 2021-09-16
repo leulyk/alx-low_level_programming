7e36864147d9f5f293a7b0fd8d218f3514c009cf
Leul Yiheyis <leulykpro@gmail.com>
2021-09-16 17:57:56 +0300

    check current if it's not null before assigning next

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -40,7 +40,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int num)
 		}
 		counter++;
 		previous = current;
-		current = current->next;
+		if (current)
+			current = current->next;
 	} while (previous);
 
 	free(newnode);
