8840bd63b361961be4fa4e83cdc2fba4ab7e1dc2
Leul Yiheyis <leulykpro@gmail.com>
2021-09-16 18:21:17 +0300

    minor fix

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,6 +27,6 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *h, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-dlistint_t *delete_dnodeint_at_index(dlistint_t **h, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **h, unsigned int index);
 
 #endif
