fc0d5a684d42dc0c1c513935cfd3b6a9f1069ddb
Leul Yiheyis <leulykpro@gmail.com>
2021-10-08 18:40:07 +0300

    use only one function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include "hash_tables.h"
 
-hash_node_t *add_hashnode(hash_node_t *current, const char *key,
-		const char *value);
 /**
  * hash_table_set - adds an element to the hash table
  *
@@ -18,46 +16,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newnode;
 	unsigned long int index;
-	char *val;
 
 	if (key == NULL || strcmp(key, "") == 0)
 		return (0);
-	val = strdup(value);
-	if (val == NULL)
-		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
-	newnode = add_hashnode(ht->array[index], key, val);
+
+	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
 		return (0);
 
-	return (1);
-}
-
-/**
- * add_hashnode - add a new hash node to the hash table array
- *
- * @current: pointer to the current index in the array
- * @key: key to assign to the new node
- * @value: value of the new node
- *
- * Return: pointer to the new node, NULL if it fails
- *
- */
-hash_node_t *add_hashnode(hash_node_t *current, const char *key,
-		const char *value)
-{
-	hash_node_t *new;
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (NULL);
-	new->key = strdup(key);
-	if (new->key == NULL)
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+	if (!(newnode->key) || !(newnode->value))
 		return (0);
-	new->value = (char *)value;
-	if (!current)
-		new->next = current;
-	current = new;
 
-	return (new);
+	if (ht->array[index])
+		newnode->next = ht->array[index];
+	ht->array[index] = newnode;
+
+	return (1);
 }
