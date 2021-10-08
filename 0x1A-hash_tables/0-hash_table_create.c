b1d4329b96a25698016fee929c5c2a8fdfcc514e
Leul Yiheyis <leulykpro@gmail.com>
2021-10-08 19:47:28 +0300

    Minor fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,13 +16,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t));
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
+	table->size = size;
 
 	return (table);
 }
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newnode;
 	unsigned long int index;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (key == NULL || strcmp(key, "") == 0 || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
