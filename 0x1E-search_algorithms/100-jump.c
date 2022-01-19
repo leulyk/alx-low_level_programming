b2baf488fc38cf9e45e675c8709d950ae7148ffd
Leul Yiheyis <leulykpro@gmail.com>
2022-01-19 14:49:58 +0300

    implementation of jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+#include "search_algos.h"
+
+
+/**
+ * jump_search - search an array of integers using Jump Search algorithm
+ *
+ * @array: pointer to the first element of the array to search
+ * @size: size of the array to search
+ * @value: value to search for
+ *
+ * Return: the first index the value is found at, otherwise -1 if array
+ * is null or value can't be found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, j, block_size;
+
+	if (!array)
+		return (-1);
+
+	block_size = (size_t)sqrt((double)size);
+	for (i = 0; i <= block_size; ++i)
+	{
+		if (array[i * block_size] >= value)
+		{
+			if (i == 0)
+				return (-1);
+			printf("Value found between indexes [%ld] and [%ld]\n",
+				(i - 1) * block_size, i * block_size);
+			for (j = (i - 1) * block_size; j <= (i * block_size); j++)
+			{
+				printf("Value checked array [%ld] = [%d]\n", j,
+					array[j]);
+				if (array[j] == value)
+					return (j);
+			}
+			return (-1);
+		}
+		printf("Value checked array [%ld] = [%d]\n", i * block_size,
+			array[i * block_size]);
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n",
+		(i - 1) * block_size, size);
+	for (j = (i - 1) * block_size; j < size; ++j)
+	{
+		printf("Value checked array [%ld] = [%d]\n", j,
+			array[j]);
+		if (array[j] == value)
+			return (j);
+	}
+
+	return (-1);
+}
